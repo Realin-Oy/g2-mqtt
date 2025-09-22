d34b31234b641d5aca9037f3135989d99b5cc344
Janne Tervo <janne.tervo@realin.fi>
2025-09-22 14:44:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+G2 MQTT Encoder
+===
+
+Build
+--
+
+Packaging system uses [uv](https://docs.astral.sh/uv/)
+
+uv sync
+uv build
+
+Usage
+---
+
+```python
+from g2_mqtt.g2_encoder import G2Encoder
+
+encoder = G2Encoder()
+
+measurements = [
+    (1758536355, {'datatype': 'temperature', 'unit': 'C', 'value': 23.1, 'time': 0}),
+    (1758536355, {'datatype': 'humidity', 'unit': '%', 'value': 45, 'time': 'ignored'}),
+]
+
+# Create a list of bytes-types packages
+bytes_values = list(encoder.encode(measurements))
+
+# Parse each package to measurement lists
+measurements = []
+for item in bytes_values:
+    measurements += list(encoder.decode(item))
+
+"""
+Results to ->
+measurements = [
+    {'datatype': 'temperature', 'time': 1758536355, 'unit': 'C', 'value': 23.100000381469727},
+    {'datatype': 'humidity', 'time': 1758536355, 'unit': '%', 'value': 45.0}
+]
+"""
+```
+
+Protocol
+---
+
+Message consists of a 5 byte header of (version, unix timestamp)
+and repeating 5 byte payload of (channel, value)
+
+Several messages of this form can be concatenated to one packet.
+
+Data is sent to mqtt topic g2/{network}/{serial}
+
+<img src="packet.svg">
+
+```mermaid
+---
+title: "Header 5 bytes, payload* 5bytes (repeating)"
+---
+packet
++8: "Version uint:8 (0..0x64) = 0x01"
++32: "Unix Timestamp uintbe:32"
++8: "Channel uint:8 (0x65..0xFF)"
++32: "Value floatbe:32"
++8: "Channel uint:8 (0x65..0xFF)"
++32: "Value floatbe:32"
+```
+
---
+++ g2_mqtt/g2_encoder.py
@@ -0,0 +1,162 @@
+import logging
+from typing import Generator, Iterable, TypedDict
+
+from bitstring import BitStream, ConstBitStream
+
+logger = logging.getLogger(__name__)
+
+"""
+Header:
+version: uint8  # 1..100 =  0x01..0x64 int
+timestamp: uint32 big endian  # unix timestamp
+
+repeat payload:
+    byte channel, starts from 101
+    float32 big endian value
+    
+```mermaid
+
+---
+title: "Header (5 bytes)"
+---
+packet
++8: "Version uint:8 (0..0x64) = 0x01"
++32: "Unix Timestamp uintbe:32"
+```
+
+Channel datatype and unit are transmitted out of band
+
+```mermaid
+---
+title: "payload (5 bytes per channel)"
+---
+packet
++8: "channel uint:8 (0x65..0xFF)"
++32: "Value floatbe:32"
+```
+
+# Example packet size: g2 measurements 11 channels x 5 bytes = 55 bytes + version + timestamp = 60 bytes
+"""
+
+MQTT_ENCODER = 'g2'
+MQTT_TOPIC = 'g2/{network}/{node}'
+
+
+TYPE_FORMAT = 'uint:8'
+TIMESTAMP_FORMAT = 'uintbe:32'
+VALUE_FORMAT = 'floatbe:32'
+
+G2_VERSION = 1
+
+CHANNELS = [
+    # Reference to IPSO smart objects
+    'version,=1',
+
+    'temperature,C=103',
+    'humidity,%=104',
+    'barometer,Pa=115',
+
+    'pegasor:PN,n/cm^3=120',
+    'pegasor:OmeFT,=121',
+    'pegasor:humidity,%=122',
+    'pegasor:FeedPressure,kPa=123',
+    'pegasor:ambient_temperature,C=124',
+    'pegasor:board_temperature,C=125',
+    'pegasor:CMD,nm=126',
+    'pegasor:error,=127',
+    'pegasor:PM,µg/m^3=128',
+    'pegasor:LDSA,µm^2/cm^3=129',
+    'pegasor:PN_uncut,n/cm^3=130',
+
+    'lat,deg=136',
+    'lon,deg=137',
+
+    # 'pump:freq,kHz=140',
+]
+
+# Map from datatype to channel number
+ENCODE_CHANNELS = {item.split(',')[0]: int(item.split('=')[1]) for item in CHANNELS}
+
+# Map from channel number to datatype, unit
+DECODE_CHANNELS = {int(item.split('=')[1]): item.split('=')[0] for item in CHANNELS}
+
+
+class Measurement(TypedDict):
+    datatype: str
+    value: float
+    unit: str
+    time: float | None
+
+
+Item = tuple[float, Measurement]
+
+
+class G2Encoder:
+    version = G2_VERSION
+    encode_channels = ENCODE_CHANNELS
+    decode_channels = DECODE_CHANNELS
+    PART_SIZE_BITS = 40
+
+    verbose = True
+
+    def encode(self, measurements: Iterable[Item]) -> Generator[bytes, None, None]:
+        # presumes time-sorted measurements
+        previous = None
+
+        payload = BitStream()
+
+        for at, measurement in measurements:
+            # unit = measurement['unit']
+            # timestamp = measurement['time']
+            channel = measurement['datatype']
+            value = measurement['value']
+
+            if previous is None or at != previous:
+                if len(payload.bytes) > 5:
+                    yield payload.bytes
+                payload = BitStream()
+                payload.append(f'uint:8={self.version}')
+                payload.append(f'{TIMESTAMP_FORMAT}={int(at)}')
+
+            data_channel = self.encode_channels.get(channel)
+            if data_channel:
+                payload.append(f'{TYPE_FORMAT}={int(data_channel)}')
+                payload.append(f'{VALUE_FORMAT}={value}')
+            else:
+                if self.verbose:
+                    logger.debug(f'Unknown {self.version=} channel {channel}={value}')
+
+            previous = at
+
+        if payload and len(payload.bytes) > 5:
+            yield payload.bytes
+
+    def decode(self, payload: bytes) -> Generator[
+        Measurement, None, None]:
+        stream = ConstBitStream(payload)
+        bits = len(stream)
+
+        while True:
+            if stream.bitpos > (bits - self.PART_SIZE_BITS):
+                break
+
+            version = stream.read(TYPE_FORMAT)
+
+            if self.verbose and version != self.version:
+                logger.warning(f'Unknown {version=} (expected {self.version})')
+
+            timestamp: int = stream.read(TIMESTAMP_FORMAT)
+
+            bits = len(stream)
+
+            while stream.bitpos != bits:
+                channel = stream.read(TYPE_FORMAT)
+                value: float = stream.read(VALUE_FORMAT)
+                data_channel = self.decode_channels.get(channel)
+
+                if data_channel:
+                    datatype, unit = data_channel.split(',')
+                    yield Measurement(time=timestamp, datatype=datatype, value=value, unit=unit)
+                else:
+                    if self.verbose:
+                        logger.debug(f'Unknown {version=} (expected {self.version}) channel {channel}={value}')
